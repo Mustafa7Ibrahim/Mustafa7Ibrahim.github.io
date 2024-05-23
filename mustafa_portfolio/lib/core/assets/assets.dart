3eeec66984e17c7ab91a1e978827eae3b0c27ed0
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-05-23 17:49:34 +0300

    refactor: Remove unused profile picture path

--- mustafa_portfolio/lib/core/assets/assets.dart
+++ mustafa_portfolio/lib/core/assets/assets.dart
@@ -1,8 +1,5 @@
 /// Assets class for all assets
 class Assets {
-  /// my profile picture path
-  static String get me => 'assets/me.jpg';
-
   /// dart logo path
   static String get dartLogo => 'assets/dart.png';
 }
