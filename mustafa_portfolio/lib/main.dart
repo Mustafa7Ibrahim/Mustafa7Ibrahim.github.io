7900149e77e181f4c46eb08c39b464cf86bcb8c6
Mustafa Ibrahim <Mustafa7ibrahim@gmail.com>
2024-05-23 17:50:31 +0300

    refactor: Remove unused profile picture path

--- mustafa_portfolio/lib/main.dart
+++ mustafa_portfolio/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mustafa_portfolio/core/bloc/bloc_observer.dart';
 import 'package:mustafa_portfolio/core/config/routes/route_config.dart';
 import 'package:mustafa_portfolio/core/config/theme/app_theme.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -29,16 +28,6 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.appTheme,
       debugShowCheckedModeBanner: false,
       routerConfig: RouteConfig.router,
-      builder: (context, child) {
-        return ResponsiveBreakpoints.builder(
-          child: child!,
-          breakpoints: [
-            const Breakpoint(start: 0, end: 450, name: MOBILE),
-            const Breakpoint(start: 451, end: 800, name: TABLET),
-            const Breakpoint(start: 801, end: double.infinity, name: DESKTOP),
-          ],
-        );
-      },
     );
   }
 }
