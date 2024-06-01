4917d56dac357403de87426865bf2e86e65de36f
Mustafa Ibrahim <mustafa7ibrahim@gmail.com>
2024-06-02 00:10:32 +0300

    Refactor home screen to separate projects section into its own component

--- mustafa_portfolio/lib/modules/home/views/home_wrapper.dart
+++ mustafa_portfolio/lib/modules/home/views/home_wrapper.dart
@@ -1,15 +1,12 @@
-import 'dart:ui';
-
-import 'package:easix/easix.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:mustafa_portfolio/core/config/theme/sizes.dart';
-import 'package:mustafa_portfolio/modules/home/views/components/articles_section.dart';
-import 'package:mustafa_portfolio/modules/home/views/components/projects_section.dart';
+import 'package:mustafa_portfolio/core/config/theme/app_colors.dart';
+import 'package:mustafa_portfolio/core/responsive/responsive_layouts.dart';
 import 'package:mustafa_portfolio/modules/home/bloc/portfolio_bloc.dart';
-import 'package:mustafa_portfolio/modules/home/views/components/packages_section.dart';
-import 'package:mustafa_portfolio/modules/home/views/widgets/resume_screen.dart';
+import 'package:mustafa_portfolio/modules/home/views/home_wrapper_compact.dart';
+import 'package:mustafa_portfolio/modules/home/views/home_wrapper_medium.dart';
+import 'package:mustafa_portfolio/modules/home/views/widgets/web_nav.dart';
 
 ///
 class HomeWrapper extends HookWidget {
@@ -29,117 +26,72 @@ class HomeWrapper extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final homeKey = useMemoized(() => GlobalKey());
+    final experienceKey = useMemoized(() => GlobalKey());
     final projectsKey = useMemoized(() => GlobalKey());
-    final aboutMeKey = useMemoized(() => GlobalKey());
     final packagesKay = useMemoized(() => GlobalKey());
-    final articlesKey = useMemoized(() => GlobalKey());
+    final blogKey = useMemoized(() => GlobalKey());
+    final skillsKey = useMemoized(() => GlobalKey());
+    final aboutMeKey = useMemoized(() => GlobalKey());
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(180),
-        child: ClipRRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: designPadding,
-                vertical: designPaddingCenter,
-              ),
-              decoration: new BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-              ),
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () => goToWidgetByTag(homeKey),
-                    child: Text(
-                      'Mustafa Ibrahim',
-                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                  ),
-                  const Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      goToWidgetByTag(projectsKey);
-                    },
-                    child: const Text('Work'),
-                  ),
-                  designPaddingCenter.pw,
-                  TextButton(
-                    onPressed: () {
-                      goToWidgetByTag(packagesKay);
-                    },
-                    child: const Text('Packages'),
-                  ),
-                  designPaddingCenter.pw,
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Skills'),
-                  ),
-                  designPaddingCenter.pw,
-                  TextButton(
-                    onPressed: () {
-                      goToWidgetByTag(articlesKey);
-                    },
-                    child: const Text('Blog'),
-                  ),
-                  designPaddingCenter.pw,
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Contact'),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: designPadding,
-                        vertical: designPadding20,
-                      ),
-                    ),
-                  ),
-                ],
+    return BlocProvider(
+      create: (context) => PortfolioBloc()..add(const LoadPortfolioEvent()),
+      child: BlocBuilder<PortfolioBloc, PortfolioState>(
+        builder: (context, state) {
+          if (state.status == PortfolioStatus.loading) {
+            return Scaffold(
+              body: const Center(child: CircularProgressIndicator()),
+            );
+          }
+          return Scaffold(
+            extendBodyBehindAppBar: true,
+            backgroundColor: AppColors.backgroundColor,
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(200),
+              child: WebNavBar(
+                experienceKey: experienceKey,
+                projectsKey: projectsKey,
+                packagesKay: packagesKay,
+                articlesKey: blogKey,
+                skillsKey: skillsKey,
+                goToWidgetByTag: goToWidgetByTag,
               ),
             ),
-          ),
-        ),
-      ),
-      body: BlocProvider(
-        create: (context) => PortfolioBloc()..add(const LoadPortfolioEvent()),
-        child: BlocBuilder<PortfolioBloc, PortfolioState>(
-          builder: (context, state) {
-            if (state.status == PortfolioStatus.loading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  ProjectsSection(
-                    key: homeKey,
-                    projectsKey: projectsKey,
-                    projects: state.projects,
-                    onTapWork: () => goToWidgetByTag(projectsKey),
-                    onTapAbout: () => goToWidgetByTag(aboutMeKey),
-                  ),
-                  PackagesSection(
-                    key: packagesKay,
-                    packages: state.packages,
-                  ),
-                  ResumeScreen(
-                    key: aboutMeKey,
-                    state: state,
-                  ),
-                  ArticlesScreen(
-                    key: articlesKey,
-                    articles: state.articles,
-                  ),
-                ],
+            body: WindowClassLayout(
+              compact: (context) => HomeWrapperCompact(
+                experienceKey: experienceKey,
+                projectsKey: projectsKey,
+                packagesKay: packagesKay,
+                blogKey: blogKey,
+                skillsKey: skillsKey,
+                aboutMeKey: aboutMeKey,
+                goToWidgetByTag: goToWidgetByTag,
+                state: state,
+                windowSizeResponsive: WindowSizeResponsive.compact,
               ),
-            );
-          },
-        ),
+              medium: (context) => HomeWrapperMedium(
+                experienceKey: experienceKey,
+                projectsKey: projectsKey,
+                packagesKay: packagesKay,
+                blogKey: blogKey,
+                skillsKey: skillsKey,
+                aboutMeKey: aboutMeKey,
+                goToWidgetByTag: goToWidgetByTag,
+                state: state,
+                windowSizeResponsive: WindowSizeResponsive.medium,
+              ),
+              // expanded: (context) => HomeWrapperExpanded(
+              //   experienceKey: experienceKey,
+              //   projectsKey: projectsKey,
+              //   packagesKay: packagesKay,
+              //   blogKey: blogKey,
+              //   skillsKey: skillsKey,
+              //   aboutMeKey: aboutMeKey,
+              //   goToWidgetByTag: goToWidgetByTag,
+              //   state: state,
+              // ),
+            ),
+          );
+        },
       ),
     );
   }
